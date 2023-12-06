f8ac0ba6f639c4a2ec555d434fab186c2a8a65d2
antoinehabis <antoine.habis@telecom-paristech.fr>
2023-12-06 17:40:08 +0100

    add the contour to distance map layer

--- torch_contour/__init__.py
+++ torch_contour/__init__.py
@@ -1,4 +1,4 @@
 from.contour_to_mask import Contour_to_mask
 
-__all__ = ["Contour_to_mask"]
-__version__ = "0.0.1"
+__all__ = ["Contour_to_mask","Contour_to_distance_map"]
+__version__ = "0.0.2"
--- torch_contour/torch_contour.py
+++ torch_contour/torch_contour.py
@@ -36,3 +36,41 @@ class Contour_to_mask(nn.Module):
         mask = torch.unsqueeze(out0, dim=0)
 
         return mask
+
+
+
+class Contour_to_distance_map(nn.Module):
+    def __init__(self, size, k=1e5, eps=1e-5):
+        super().__init__()
+        self.k = k
+        self.eps = eps
+        self.size = size
+        self.mesh = torch.unsqueeze(
+            torch.stack(
+                torch.meshgrid(torch.arange(self.size), torch.arange(self.size)),
+                dim=-1,
+            ).reshape(-1, 2),
+            dim=1,
+        )
+
+    def forward(self, contour):
+
+        
+        contour = torch.unsqueeze(contour, dim=0)
+        diff = -self.mesh + contour
+        roll_diff = torch.roll(diff, -1, dims=1)
+        sign = diff * torch.roll(roll_diff, 1, dims=2)
+        sign = sign[:, :, 1] - sign[:, :, 0]
+        sign = torch.tanh(self.k * sign)
+        norm_diff = torch.norm(diff, dim=2)
+        norm_roll = torch.norm(roll_diff, dim=2)
+        scalar_product = torch.sum(diff * roll_diff, dim=2)
+        clip = scalar_product / (norm_diff * norm_roll + self.eps)
+        angles = torch.arccos(torch.clip(clip, eps, 1 - self.eps))
+        torch.pi = torch.acos(torch.zeros(1)).item() * 2
+        sum_angles = torch.clip(-torch.sum(sign * angles, dim=1) / (2 * torch.pi), 0, 1)
+        out0 = sum_angles.reshape(1, self.size, self.size)
+        mask = torch.unsqueeze(out0, dim=0)
+
+        return mask
+
