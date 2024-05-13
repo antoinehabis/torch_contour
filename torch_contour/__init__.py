3096f95d8ec154520a0ff2e591e4fee33e0f39cc
antoinehabis <antoine.habis@telecom-paristech.fr>
2024-05-13 14:43:26 +0200

    add documentation

--- torch_contour/__init__.py
+++ torch_contour/__init__.py
@@ -1,4 +1,4 @@
-# from.contour_to_mask import Contour_to_mask
+from .torch_contour import Contour_to_mask, Contour_to_distance_map
 
-# __all__ = ["Contour_to_mask","Contour_to_distance_map"]
-# __version__ = "0.0.2"
+__all__ = ["Contour_to_mask","Contour_to_distance_map"]
+__version__ = "0.0.3"
