a597b60dc7120565ef242146115c17d8e05b88a8
antoinehabis <antoine.habis@telecom-paristech.fr>
2024-06-27 17:24:41 +0200

    add methods for perimeter area and haussdorf distance

--- torch_contour/__init__.py
+++ torch_contour/__init__.py
@@ -1,4 +1,4 @@
 from .torch_contour import Contour_to_mask, Contour_to_distance_map
 
-__all__ = ["Contour_to_mask","Contour_to_distance_map", "area", "perimeter", "haussdorf_distance"]
+__all__ = ["Contour_to_mask","Contour_to_distance_map", "area", "perimeter", "haussdorff_distance"]
 __version__ = "1.0.4"
